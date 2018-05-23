85fdb6c5e754b414f34ce30d0f8aa28403961279
Madhura Bhave <mbhave@pivotal.io>
2018-05-23 18:27:49 +0200

    Add micrometer

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,7 +1,10 @@
 package com.example.demo;
 
+import io.micrometer.core.instrument.MeterRegistry;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.core.CollectionOptions;
@@ -12,6 +15,12 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public MeterRegistryCustomizer<MeterRegistry> commonTags() {
+		return (registry) -> registry.config()
+				.commonTags("application", "demo");
 	}
 
 	@Bean
